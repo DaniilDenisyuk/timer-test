e75edf3d9164a405e2b394eeb79844d806c84f9f
Daniil Denisyuk <ordekonec@gmail.com>
2021-04-06 23:25:01 +0300

    finish

--- timer-app/src/components/Timer.js
+++ timer-app/src/components/Timer.js
@@ -1,22 +1,6 @@
 import cn from "classnames";
 import { useState } from "react";
-import { Observable } from "rxjs";
-
-const Timer = () => {
-  const step = () => {
-    var dt = Date.now() - expected; // the drift (positive for overshooting)
-    if (dt > interval) {
-      // something really bad happened. Maybe the browser (tab) was inactive?
-      // possibly special handling to avoid futile "catch up" run
-    }
-    expected += interval;
-    setTimeout(step, Math.max(0, interval - dt)); // take into account drift
-  };
-  return new Observable((subscriber) => {
-    setInterval();
-    subscriber.next();
-  });
-};
+import { Subject } from "rxjs";
 
 const parseHours = (callback) => (time) => {
   const hours = Math.floor(time / (1000 * 60 * 60));
@@ -33,26 +17,97 @@ const parseSeconds = (callback) => (time) => {
   callback(seconds);
 };
 
+function Timer(...observers) {
+  const timerSubject = new Subject();
+  observers.forEach((observer) => timerSubject.subscribe(observer));
+
+  let interval = 1000;
+  let timeout;
+  let startTime;
+  let elapsedTime;
+  let expected;
+  const step = () => {
+    const now = Date.now();
+    var dt = now - expected;
+    if (dt > interval) {
+    }
+    timerSubject.next(now - startTime);
+    console.log(timeout);
+    expected += interval;
+    timeout = setTimeout(step, Math.max(0, interval - dt));
+  };
+
+  const start = () => {
+    const now = Date.now();
+    expected = now + interval;
+    startTime = now;
+    timeout = setTimeout(step, interval);
+  };
+  const stop = () => {
+    clearTimeout(timeout);
+    elapsedTime = Date.now() - startTime;
+  };
+  const reset = () => {
+    clearTimeout(timeout);
+    timerSubject.next(0);
+  };
+  const restart = () => {
+    clearTimeout(timeout);
+    timerSubject.next(0);
+    startTime = Date.now();
+    timeout = setTimeout(step, 0);
+  };
+  const resume = () => {
+    startTime = Date.now() - elapsedTime;
+    timeout = setTimeout(step, interval);
+  };
+  return {
+    reset,
+    start,
+    stop,
+    restart,
+    resume,
+  };
+}
+
 const TimerComponent = () => {
+  const [timer] = useState(
+    new Timer(
+      parseSeconds(handleSecondsChange),
+      parseMinutes(handleMinutesChange),
+      parseHours(handleHoursChange)
+    )
+  );
   const [isTimerActive, setIsTimerActive] = useState(false);
   const [isTimerStopped, setIsTimerStopped] = useState(false);
   const [seconds, setSeconds] = useState("00");
   const [minutes, setMinutes] = useState("00");
   const [hours, setHours] = useState("00");
 
-  const handleHoursChange = (hours) => {
+  async function handleHoursChange(hours) {
     setHours(("0" + hours).slice(-2));
-  };
-  const handleMinutesChange = (minutes) => {
+  }
+  async function handleMinutesChange(minutes) {
     setMinutes(("0" + minutes).slice(-2));
-  };
-  const handleSecondsChange = (seconds) => {
+  }
+  async function handleSecondsChange(seconds) {
     setSeconds(("0" + seconds).slice(-2));
-  };
+  }
 
   const handleStartStopClick = () => {
-    if (isTimerActive) timer.reset();
-    else timer.start();
+    if (isTimerStopped) {
+      console.log("resume");
+      timer.resume();
+      setIsTimerStopped(false);
+      return;
+    }
+    if (isTimerActive) {
+      console.log("reset");
+      timer.reset();
+    } else {
+      console.log("start");
+      timer.start();
+    }
     setIsTimerActive(!isTimerActive);
   };
 
@@ -66,6 +121,7 @@ const TimerComponent = () => {
         setTimeout(() => (firstClick = true), 300);
       } else if (e.timeStamp - t1 < 300) {
         timer.stop();
+        setIsTimerStopped(true);
       }
     };
   })();
@@ -74,15 +130,16 @@ const TimerComponent = () => {
     timer.restart();
   };
 
-  const timer = new Timer(
-    parseSeconds(handleSecondsChange),
-    parseMinutes(handleMinutesChange),
-    parseHours(handleHoursChange)
-  );
-
   return (
     <section className="timer">
-      <h2 className="timer__heading"></h2>
+      <h2 className="timer__heading">Таймер</h2>
+      {isTimerStopped ? (
+        <p className="timer__info">Таймер остановлен</p>
+      ) : isTimerActive ? (
+        <p className="timer__info">Таймер запущен</p>
+      ) : (
+        <p className="timer__info">Таймер не запущен</p>
+      )}
       <div className="timer__digits">
         <span className="timer__digit">{hours}</span>:
         <span className="timer__digit">{minutes}</span>:
@@ -91,8 +148,8 @@ const TimerComponent = () => {
       <div className="timer__buttons">
         <button
           className={cn("timer__button ", {
-            "timer__button--start": !isTimerActive,
-            "timer__button--stop": isTimerActive,
+            "timer__button--start": !isTimerActive || isTimerStopped,
+            "timer__button--stop": isTimerActive && !isTimerStopped,
           })}
           onClick={handleStartStopClick}
         ></button>
